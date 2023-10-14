00e7069c5d6bf513325a2dd1de41d2f27cf5c0ea
VieiraLaris <lalasarandi@gmail.com>
2023-10-13 23:48:05 -0300

    :bulb:(#35): adição da logo no menuBar

--- implementacao-front/src/components/MenuBar.tsx
+++ implementacao-front/src/components/MenuBar.tsx
@@ -1,9 +1,13 @@
 import styles from './MenuBar.module.css'
+import logo from "./imagens/logo.png"
 
 function MenuBar() {
   return (
     <div className={styles.menuBar}>
-      
+      <br></br>
+      <div className={styles.logo}>
+        <img src={logo} className="logo" alt="" width="220" height="185"/>
+      </div>
     </div>
   )
 }

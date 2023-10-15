9ee8734f70b50d228822740abc965b34fe666179
vitor feijo leonardo <vitorfleonardo@gmail.com>
2023-10-15 16:33:50 -0300

    :bulb:(#34): Adicionando botões no menu bar

--- implementacao-front/src/components/MenuBar.tsx
+++ implementacao-front/src/components/MenuBar.tsx
@@ -8,6 +8,13 @@ function MenuBar() {
       <div className={styles.logo}>
         <img src={logo} className="logo" alt="" width="220" height="185"/>
       </div>
+      <div className={styles.links}>
+        <a href="url">Home</a>
+        <a href="url">Pesquisa</a>
+        <a href="url">Municípios</a>
+        <a href="url">Anomalias</a>
+        <a href="url">Sobre</a>
+      </div>
     </div>
   )
 }

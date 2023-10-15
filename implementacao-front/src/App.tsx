d2ec80dd8458886cd4796fb7ad63ea7b6ac7a152
anabborges <abarbosaborges17@gmail.com>
2023-10-15 15:51:24 -0300

    :bulb:(#34): criacao de rotas

--- implementacao-front/src/App.tsx
+++ implementacao-front/src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './pages/About'
+import Home from './pages/Home';
 
 function App() {
 
   return (
-    <>
-      <About />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ implementacao-front/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import MenuBar from "../components/MenuBar"
+
+function Home() {
+  return (
+    <div>
+      <MenuBar />
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+export default Home

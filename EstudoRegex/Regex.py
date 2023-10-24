9f6907d5fdf603c84720a5afcdab1a1a8afc64f6
Bruno <bdebatatadoce@gmail.com>
2023-10-23 22:54:25 -0300

    :repeat:(#50): Regex capaz de pegar o nome do municipio, com erros

--- EstudoRegex/Regex.py
+++ EstudoRegex/Regex.py
@@ -1,46 +1,60 @@
 import regex as re
+""""
+        {
+            municipio: nome do munucipio
+            data: data do dia de extração
+            nomeação: bool indicando se há 
+            extração: bool indicando se há
+            nomeado: nome/cargo/cpf ou None 
+            exonerado: nome/cargo/cpf ou None 
 
-
-def pega_inf(bloco):
-    lista_sobre = []
-    nomes = []
-    for letra in blocos[1:]:
-        palavra = letra
-        for l in range(0, len(palavra)):
-            if palavra[l] == "S" and palavra[l+1] == "r" and palavra[l+2] == ".":
-                informacoes= palavra[l:70+l]
-                nome = palavra[l:l+10]
-                if nome in nomes:
-                    print("Já coloquei")
-                else:
-                    nomes.append(nome)
-                    lista_sobre.append(informacoes)
-
-    print("Nomes de concursados:")          
-    print(nomes)
-    print("Cargo para exercer")
-    print(lista_sobre)
-
-
+        }
+"""
 
 with open("2011-01-04.txt", 'r') as arquivo: 
     texto = arquivo.read()
-    padrao = re.escape("PREFEITURA MUNICIPAL DE ")
-    correspondencias = re.finditer(padrao, texto, re.DOTALL | re.IGNORECASE)
+    padrao = re.escape("PREFEITURA MUNICIPAL ")
     blocos = re.split(padrao, texto)
-    lista_de_informacoes = []
+    # Data
+    padrao = r'\d{2} de [A-Z][a-z]+ de \d{4}'
+    datas_encontradas = re.findall(padrao, texto)
+    data = datas_encontradas.pop()
 
     for bloco in blocos[1:]:
         bloco = bloco.strip()
-        #print(bloco)
+        nomeacao = None
+        exoneracao = None
+        # Municipio 
+        padraoMunicipio = r'DE(.*?)\n'
+        municipio = re.search(padraoMunicipio, bloco)
+        nomeDoMunicipio = municipio.group(1).strip()
+        
         if re.search(re.escape("nomeia"), bloco, re.IGNORECASE):
-            print("Há nomeação! Data: ")
-            pega_inf(bloco)
-    
-            lista_de_informacoes.append(bloco)
+            entreNome = r"Sr\. (.*?),"
+            nomeado = re.search(entreNome, texto)
+            nomeDoNomeado = nomeado.group(1).strip()
+            
+            padraoCargo = r"para exercer o Cargo de (.*?)(?:\n|$)"
+            cargo = re.search(padraoCargo, texto)
+            cargoNomeado = cargo.group(1).strip()
+            print(cargoNomeado)
+            nomeacao = True
         else:
-            print(f'Não há nomeação.')
+            nomeacao = False
 
-
-
+        if re.search(re.escape("exonera"), bloco, re.IGNORECASE):
+            exoneracao = True
+        else:   
+            exoneracao = False
+            
+        # Escrita no .json   
+        print("Separação")      
+        print(data)
+        print("Municipio: ", nomeDoMunicipio)
+        print("Há nomeação: ", nomeacao)
+        if nomeacao == True:
+            print("Nome do nomeado: ", nomeDoNomeado)
+            print("Cargo do nomeado: ", cargoNomeado)
+        print("Há exoneração: ", exoneracao)
+        print('\n\n')
     

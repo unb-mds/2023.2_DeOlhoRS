28df3013659672810ea3b06b02be8c07445ceb46
anabborges <abarbosaborges17@gmail.com>
2023-11-11 00:50:12 -0300

    :bulb:(#62): criacao do input e botao para pesquisa

--- implementacao-front/src/pages/PesquisaAvancada.tsx
+++ implementacao-front/src/pages/PesquisaAvancada.tsx
@@ -2,8 +2,13 @@ import MenuBar from "../components/MenuBar";
 import styles from "./PesquisaAvancada.module.css";
 //import TesteTabela from '../components/TesteTabela.tsx';
 import Tabela from "../components/Tabela";
+import { useState } from "react";
+import { MDBBtn, MDBBtnGroup } from "mdb-react-ui-kit";
 
 function PesquisaAvancada() {
+
+  const [value, setValue] = useState("")
+
   return (
     <div className={styles.container}>
       <MenuBar />
@@ -11,7 +16,15 @@ function PesquisaAvancada() {
         <h1 className={styles.titulo}>Pesquisa Avançada</h1>
         <TesteTabela />
       </div>*/}
-      <Tabela></Tabela>
+      <div className={styles.content}>
+        <form>
+          <input type="text" placeholder="Nome..." value={value} onChange={(e) => setValue(e.target.value)}/>
+          <MDBBtnGroup>
+            <MDBBtn>Pesquisar</MDBBtn>
+          </MDBBtnGroup>
+        </form>
+        <Tabela></Tabela>
+      </div>
     </div>
   );
 }

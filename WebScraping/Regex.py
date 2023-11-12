258f443d8065f80f0ea4d07af8010bf0391eacfe
Bruno <bdebatatadoce@gmail.com>
2023-11-12 20:22:13 -0300

    :cool:(#61): Alterações no regex e obtenção de um ano de pdfs convertidos para txt
    Co-authored-by: Bianca Patrocínio <BiancaPatrocinio7@users.noreply.github.com>

--- WebScraping/Regex.py
+++ WebScraping/Regex.py
@@ -11,6 +11,15 @@ import json
 class ExtratorDeDados:
     def __init__(self):
         pass
+    def extrairNomeMunicipio(self, bloco):
+        # Extrair nome do município com flexibilidade para padrões diferentes
+        padrao_municipio = re.compile(r'(?:PREFEITURA MUNICIPAL DE\s*|MUNICÍPIO DE\s*|CÂMARA MUNICIPAL DE VEREADORES\s*\n\s*)(.*?)(?=\n)', re.IGNORECASE | re.DOTALL)
+        municipio = padrao_municipio.search(bloco)
+
+        if municipio:
+            return municipio.group(1).strip()
+        else:
+            return "Município não encontrado"
     def escritaDatabase(self, dados):
         with open("database.json", "r", encoding="utf-8") as file:
             dados_escritos = json.load(file)
@@ -22,49 +31,38 @@ class ExtratorDeDados:
     def extrairDados(self, nomeDoArquivo):
         with open(nomeDoArquivo, 'r') as arquivo: 
             texto = arquivo.read()
-            padrao = r"(?:\s*ESTADO DO RIO GRANDE DO SUL)?(.*?)(?:ESTADO DO RIO GRANDE DO SUL)"
+            padrao = r"(?:\s*ESTADO DO RIO GRANDE DO SUL)?(.*?)(?:Identificador)"
             
             blocosProcessosMunicipio = re.findall(padrao, texto, re.DOTALL)
 
-            # Data
-            padrao = r'\d{2} de [A-Z][a-z]+ de \d{4}'
-            datas_encontradas = re.findall(padrao, texto)
-            data = datas_encontradas.pop()
-
-            for bloco in blocosProcessosMunicipio[1:]:
+            for bloco in blocosProcessosMunicipio[0:]:
                 bloco = bloco.strip() 
-                nomeacao = None
-                exoneracao = None
-                
-                # Nome do Municipio 
-                padraoMunicipio = r'PREFEITURA MUNICIPAL DE\s*([A-ZÁÀÃÉÈÍÏÓÒÕÚÇ\'-]+)(?:\s+([A-ZÁÀÃÉÈÍÏÓÒÕÚÇ\'-]+))?'
-                municipio = re.search(padraoMunicipio, bloco)
+        
+                nome_do_municipio = self.extrairNomeMunicipio(bloco)
 
-                nomeDoMunicipio = municipio.group(1).strip()
-                try:
+                # Nome do Municipio 
+                
+                """try:
+                    print(nomeDoMunicipio = municipio.group(1).strip())
                     segundaPalavra = municipio.group(2).strip()
                 except:
                     pass
                 if "SECRETARIA" not in segundaPalavra and "ASSESSORIA" not in segundaPalavra and "GABINETE" not in segundaPalavra and "CONSULTORIA" not in segundaPalavra:
                     nomeDoMunicipio += " " + segundaPalavra
-
+                """
                 # Verificação de existência de nomeação com base os padrões encontrados
-                if re.search(re.escape("nomeia"), bloco, re.IGNORECASE):
-                    nomeacao = True
-                else:
-                    nomeacao = False
-                # Verificação de existência de exoneração com base os padrões encontrados
-                if re.search(re.escape("exonera"), bloco, re.IGNORECASE):
-                    exoneracao = True
-                else:   
-                    exoneracao = False
+                nomeacao = bool(re.search(re.escape("nomeia"), bloco, re.IGNORECASE))
+                exoneracao = bool(re.search(re.escape("exonera "), bloco, re.IGNORECASE))
                 # Escrita no .json   
                 # Json só para a "Contagem" de nomeações e exonerações
+                data = arquivo.name
+                data = data[:-4]
                 dados_novos = {
-                    "nomeMunicipio": nomeDoMunicipio,
-                    "dataPost": data,
+                    "nomeMunicipio": nome_do_municipio,
+                    "dataPost": data[-10:],
                     "haNomeacao": nomeacao,
                     "haExoneracao": exoneracao,
+                    
                 }
                 self.escritaDatabase(dados_novos)
 
---
+++ WebScraping/TestesRegex.py
@@ -0,0 +1,4 @@
+from Regex import ExtratorDeDados 
+regex = ExtratorDeDados()
+
+regex.extrairDados("/home/bdebatata/MétodosDeDesenvolvimentoDeSoftware/2023-2-Squad08/WebScraping/txt/2009/2009-07-03.txt")

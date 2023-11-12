49fd3e3bd39a7b9e3900825fce1af72755333f35
VieiraLaris <lalasarandi@gmail.com>
2023-11-12 14:08:58 -0300

    :bulb:(#63): implementando os gráficos com o json
    Co-authored-by: Matheus de Mello <matheusmb2003@gmail.com>

--- implementacao-front/src/pages/PesquisaMunicipios.tsx
+++ implementacao-front/src/pages/PesquisaMunicipios.tsx
@@ -2,13 +2,14 @@ import MenuBar from "../components/MenuBar"
 import Total from "../components/Total"
 import styles from "./PesquisaMunicipios.module.css"
 import Chart from "react-apexcharts"
-//import BarChart from "../components/graficoTotalPorAno";
-//import { Qtd_RS } from '../data/totalPorAno.ts';
+//import { useState } from "react"
+import { Qtd_RS } from "../data/totalPorAno"
+import { Qtd_2023 } from "../data/totalPorAno"
 
-import { DataTable } from 'primereact/datatable'
-import { useState } from 'react'
-import { FilterMatchMode } from 'primereact/api'
-import { InputText } from 'primereact/inputtext'
+//import { DataTable } from 'primereact/datatable'
+//import { FilterMatchMode } from 'primereact/api'
+//import { InputText } from 'primereact/inputtext'
+
 
 const column = {
   options: {
@@ -16,31 +17,37 @@ const column = {
       id: 'apexchart-example'
     },
     xaxis: {
-      categories: [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
+      categories: Qtd_RS.map((item) => item.ano)
     }
-  },
+  },  
   series: [{
     name: 'Nomeações',
-    data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+    data: Qtd_RS.map((item) => parseInt(item.nomeacoes)),
     color: "#FCA622",
     },
     {
       name: 'Exonerações',
-      data: [20, 60, 50, 35, 45, 55, 67, 43, 90],
+      data: Qtd_RS.map((item) => parseInt(item.exoneracoes)),
       color: "#A11208",
     }
   ]
 }
+
+const exo_2023: number[] = Qtd_2023.map((item) => parseInt(item.exoneracoes))
+const nome_2023: number[] = Qtd_2023.map((item) => parseInt(item.nomeacoes))
+const total_2023: number[] = Qtd_2023.map((item) => parseInt(item.total))
+const exo_porcen: number = (exo_2023[0] / total_2023[0]) * 100
+const nome_porcen: number = (nome_2023[0] / total_2023[0]) * 100
 
 const pie = {
   options: {
     labels: ['Nomeações', 'Exonerações'],
     colors: ["#FCA622", "#A11208"]
   },
-  series: [30, 70],
+  series: [nome_porcen, exo_porcen]
 }
 
-const total = [87465, 84577]
+const total = [exo_2023, nome_2023]
 
 function PesquisaMunicipios() {
   return (

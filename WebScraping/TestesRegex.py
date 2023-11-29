be73f4761e1f3b7ba18f78840356a6b60ae219b9
VieiraLaris <lalasarandi@gmail.com>
2023-11-28 21:42:30 -0300

    :bulb:(#79): mudança do diretório
    Co-authored-by: Ceci Quaresma <ceciquaresma2014@gmail.com>

--- WebScraping/TestesRegex.py
+++ WebScraping/TestesRegex.py
@@ -1,3 +1,3 @@
 from Regex import ExtratorDeDados 
 regex = ExtratorDeDados()
-regex.extrairDados("/home/bdebatata/MétodosDeDesenvolvimentoDeSoftware/2023-2-Squad08/WebScraping/txt/2009/2009-08-27.txt")
+regex.extrairDados("/home/larissa/2023-2-Squad08/WebScraping/txt/2009/2009-07-03.txt")

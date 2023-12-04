2e9bb9cdfd47a9360a8677d56ce0c25ad4e3d296
VieiraLaris <lalasarandi@gmail.com>
2023-12-03 21:08:22 -0300

    :bulb:(#79): armazenando nomes dos exonerados e nomeados de 2017 em um json

--- Camada_Dados/testeWord2vec.py
+++ Camada_Dados/testeWord2vec.py
@@ -1,77 +1,100 @@
 from gensim.models import Word2Vec
 import re
 import json
+from datetime import datetime, timedelta
 
-# Caminho do arquivo
-caminho_do_arquivo = "WebScraping/txt/2021/2021-08-16.txt"
+resultados_acumulados_por_data = {}
 
-try:
-    with open(caminho_do_arquivo, 'r', encoding='utf-8') as arquivo:
-        diario_oficial_text = arquivo.read()
+# Data inicial e final
+data_inicial = datetime(2017, 1, 1)
+data_final = datetime(2017, 12, 31)
 
-        # Padrões de regex para nomeações e exonerações
-        padroes_nomeacao = re.compile(r'\b(nomear|nomeia|nomeado|nomeada|nomear|nomeação de|fica nomeada|fica nomeado)\b', flags=re.IGNORECASE)
-        padroes_exoneracao = re.compile(r'\b(exonera|exonera|exonerado|exonerada|exonerar|exoneração de|fica exonerada|fica exonerado)\b', flags=re.IGNORECASE)
+# Lista de datas no formato 'YYYY-MM-DD'
+datas = [(data_inicial + timedelta(days=i)).strftime('%Y-%m-%d') for i in range((data_final - data_inicial).days + 1)]
 
-        # Encontrar correspondências no texto
-        matches_nomeacao = padroes_nomeacao.finditer(diario_oficial_text)
-        matches_exoneracao = padroes_exoneracao.finditer(diario_oficial_text)
+for data in datas:
+    caminho_do_arquivo = f"Camada_Dados/txt/2017/{data}.txt"
+    resultados_do_dia = {
+        "nomes_nomeados": [],
+        "nomes_exonerados": [],
+    }
 
-        # Extrair nomes após as correspondências, ignorando "o sr" e "a sra"
-        nomes_apos_nomeacao = []
-        nomes_apos_exoneracao = []
+    try:
+        with open(caminho_do_arquivo, 'r', encoding='utf-8') as arquivo:
+            diario_oficial_text = arquivo.read()
 
-        for match in matches_nomeacao:
-            start_index = match.end()
-            end_index = diario_oficial_text.find(',', start_index)
-            nome_chunk = diario_oficial_text[start_index:end_index].strip()
+            # Padrões de regex para nomeações e exonerações
+            padroes_nomeacao = re.compile(r'\b(nomear|nomeia|nomeado|nomeada|nomear|nomeação de|fica nomeada|fica nomeado)\b', flags=re.IGNORECASE)
+            padroes_exoneracao = re.compile(r'\b(exonera|exonera|exonerado|exonerada|exonerar|exoneração de|fica exonerada|fica exonerado)\b', flags=re.IGNORECASE)
 
-            # Ignorar "o sr" e "a sra" ao extrair nomes
-            if not re.match(r'\bo\s*sr\b|\ba\s*sra\b', nome_chunk, flags=re.IGNORECASE):
-                nomes_apos_nomeacao.append(nome_chunk)
+            # Encontrar correspondências no texto
+            matches_nomeacao = padroes_nomeacao.finditer(diario_oficial_text)
+            matches_exoneracao = padroes_exoneracao.finditer(diario_oficial_text)
 
-        for match in matches_exoneracao:
-            start_index = match.end()
-            end_index = diario_oficial_text.find(',', start_index)
-            nome_chunk = diario_oficial_text[start_index:end_index].strip()
+            # Extrair nomes após as correspondências, ignorando "o sr" e "a sra"
+            nomes_apos_nomeacao = []
+            nomes_apos_exoneracao = []
 
-            # Ignorar "o sr" e "a sra" ao extrair nomes
-            if not re.match(r'\bo\s*sr\b|\ba\s*sra\b', nome_chunk, flags=re.IGNORECASE):
-                nomes_apos_exoneracao.append(nome_chunk)
+            for match in matches_nomeacao:
+                start_index = match.end()
+                end_index = diario_oficial_text.find(',', start_index)
+                nome_chunk = diario_oficial_text[start_index:end_index].strip()
 
-        # Treinar o modelo Word2Vec
-        tokenized_text = [re.findall(r'\b\w+\b', sentence.lower()) for sentence in diario_oficial_text.split('.')]
-        model = Word2Vec(sentences=tokenized_text, vector_size=100, window=5, min_count=1, workers=4)
+                # Ignorar "o sr" e "a sra" ao extrair nomes
+                if not re.match(r'\bo\s*sr\b|\ba\s*sra\b', nome_chunk, flags=re.IGNORECASE):
+                    nomes_apos_nomeacao.append(nome_chunk)
 
-        # Encontrar palavras similares para cada nome
-        resultados_similares_nomeacao = {}
-        resultados_similares_exoneracao = {}
+            for match in matches_exoneracao:
+                start_index = match.end()
+                end_index = diario_oficial_text.find(',', start_index)
+                nome_chunk = diario_oficial_text[start_index:end_index].strip()
 
-        for nome in nomes_apos_nomeacao:
-            if nome in model.wv:
-                similares = model.wv.most_similar(nome, topn=1)
-                resultados_similares_nomeacao[nome] = similares[0][0]
+                # Ignorar "o sr" e "a sra" ao extrair nomes
+                if not re.match(r'\bo\s*sr\b|\ba\s*sra\b', nome_chunk, flags=re.IGNORECASE):
+                    nomes_apos_exoneracao.append(nome_chunk)
 
-        for nome in nomes_apos_exoneracao:
-            if nome in model.wv:
-                similares = model.wv.most_similar(nome, topn=1)
-                resultados_similares_exoneracao[nome] = similares[0][0]
+            # Treinar o modelo Word2Vec
+            tokenized_text = [re.findall(r'\b\w+\b', sentence.lower()) for sentence in diario_oficial_text.split('.')]
+            model = Word2Vec(sentences=tokenized_text, vector_size=100, window=5, min_count=1, workers=4)
 
-        # Armazenar resultados em um arquivo JSON
-        resultado_json = {
-            "nomes_apos_nomeacao": nomes_apos_nomeacao,
-            "nomes_apos_exoneracao": nomes_apos_exoneracao,
-        }
+            # Encontrar palavras similares para cada nome
+            resultados_similares_nomeacao = {}
+            resultados_similares_exoneracao = {}
 
-        with open('resultados.json', 'w', encoding='utf-8') as json_file:
-            json.dump(resultado_json, json_file, ensure_ascii=False, indent=2)
+            for nome in nomes_apos_nomeacao:
+                if nome in model.wv:
+                    similares = model.wv.most_similar(nome, topn=1)
+                    resultados_similares_nomeacao[nome] = similares[0][0]
 
-        print("Resultados armazenados em resultados.json")
+            for nome in nomes_apos_exoneracao:
+                if nome in model.wv:
+                    similares = model.wv.most_similar(nome, topn=1)
+                    resultados_similares_exoneracao[nome] = similares[0][0]
 
-except FileNotFoundError:
-    print(f"Arquivo não encontrado: {caminho_do_arquivo}")
-except Exception as e:
-    print(f"Ocorreu um erro ao ler o arquivo: {e}")
+            resultado_json = {
+                "nomes_nomeados": nomes_apos_nomeacao,
+                "nomes_exonerados": nomes_apos_exoneracao,
+            }
+
+            resultados_do_dia["nomes_nomeados"].extend(nomes_apos_nomeacao)
+            resultados_do_dia["nomes_exonerados"].extend(nomes_apos_exoneracao)
+
+    except FileNotFoundError:
+        print(f"Arquivo não encontrado: {caminho_do_arquivo}")
+    except Exception as e:
+        print(f"Ocorreu um erro ao ler o arquivo: {e}")
+
+    # Converte a string de data para o formato datetime
+    data_formatada = datetime.strptime(data, '%Y-%m-%d').strftime('%d/%m/%Y')
+
+    # Adiciona os resultados do dia ao dicionário acumulado por data
+    resultados_acumulados_por_data[data_formatada] = resultados_do_dia
+
+# Salva resultados acumulados por data em um único arquivo JSON
+with open('nomes_2017.json', 'w', encoding='utf-8') as json_file:
+    json.dump(resultados_acumulados_por_data, json_file, ensure_ascii=False, indent=2)
+
+print("Resultados acumulados por data armazenados em resultados_acumulados_por_data.json")
 
 
 

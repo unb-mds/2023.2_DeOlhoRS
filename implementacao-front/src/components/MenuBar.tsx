4a26bee504b36512779a0967774b24d22ed312d5
anabborges <abarbosaborges17@gmail.com>
2023-12-11 14:16:53 -0300

    :bug: : arrumando links da navbar

--- implementacao-front/src/components/MenuBar.tsx
+++ implementacao-front/src/components/MenuBar.tsx
@@ -10,7 +10,7 @@ function MenuBar() {
       </div>
       <div className={styles.links}>
         <a className={styles.link} href="/">Home</a>
-        <a className={styles.link} href="https://2023-2-squad08-p9gw-chx0naegn-my-team-1c098159.vercel.app/pesquisaAvancada">Pesquisa</a>
+        <a className={styles.link} href="/pesquisaAvancada">Pesquisa</a>
         <a className={styles.link} href="/pesquisaMunicipios">Municípios</a>
         <a className={styles.link} href="/anomalias">Anomalias</a>
         <a className={styles.link} href="/about">Sobre</a>

6384d641207a5798485884d6c0ca3e055f12a9e0
vitor feijo leonardo <vitorfleonardo@gmail.com>
2023-12-12 20:39:48 -0300

    :bulb:(#88): Grafico dinamico a partir da busca de um municipio

--- implementacao-front/src/pages/PesquisaMunicipios.tsx
+++ implementacao-front/src/pages/PesquisaMunicipios.tsx
@@ -1,7 +1,8 @@
 import { Autocomplete, TextField } from "@mui/material";
-import { useEffect, useState } from "react";
+import { useEffect, useState, ChangeEvent } from "react";
 import MenuBar from "../components/MenuBar";
 import styles from "./PesquisaMunicipios.module.css";
+import Chart from "react-apexcharts";
 
 interface Municipio {
   nome: string;
@@ -9,8 +10,30 @@ interface Municipio {
 
 function PesquisaMunicipios(): JSX.Element {
   const [municipios, setMunicipios] = useState<string[]>([]);
+  const [selectedMunicipio, setSelectedMunicipio] = useState<string>('');
+  const [chartData, setChartData] = useState<any>({
+    options: {
+      chart: {
+        id: 'bar-graphic'
+      },
+      xaxis: {
+        categories: [2009, 2013, 2017, 2020, 2021]
+      }
+    },
+    series: [{
+      name: 'Nomeações',
+      data: [0, 0, 0, 0, 0], // inicia com dados vazios
+      color: "#FCA622",
+      },
+      {
+        name: 'Exonerações',
+        data: [0, 0, 0, 0, 0], // inicia com dados vazios
+        color: "#A11208",
+      }
+    ]
+  });
 
-  const onSearch = async () => {
+  const apiIBGE = async () => {
     try {
       const response = await fetch(
         "https://servicodados.ibge.gov.br/api/v1/localidades/estados/RS/municipios"
@@ -22,8 +45,41 @@ function PesquisaMunicipios(): JSX.Element {
     }
   };
   useEffect(() => {
-    onSearch();
+    apiIBGE();
   }, []);
+
+    // função para gerar dados aleatórios para os municípios
+    const generateRandomData = () => {
+      const nomeacoes = [];
+      const exoneracoes = [];
+      for (let i = 0; i < 5; i++) {
+        nomeacoes.push(Math.floor(Math.random() * 1000)); // gera um número inteiro entre 0 e 999
+        exoneracoes.push(Math.floor(Math.random() * 1000)); // gera um número inteiro entre 0 e 999
+      }
+      return {nomeacoes, exoneracoes};
+    };
+
+    const handleMunicipio = (event: ChangeEvent<{}>, selectedValue: string) => {
+      if (selectedValue) {
+        setSelectedMunicipio(selectedValue);
+        console.log(selectedValue); // mostra o município selecionado no console
+        const {nomeacoes, exoneracoes} = generateRandomData(); // gera dados aleatórios para o município
+        setChartData({ // atualiza o estado do gráfico com os novos dados
+          ...chartData,
+          series: [{
+            name: 'Nomeações',
+            data: nomeacoes,
+            color: "#FCA622",
+            },
+            {
+              name: 'Exonerações',
+              data: exoneracoes,
+              color: "#A11208",
+            }
+          ]
+        });
+      }
+    };
 
   return (
     <div className={styles.container}>
@@ -39,13 +95,25 @@ function PesquisaMunicipios(): JSX.Element {
                 disablePortal
                 id="combo-box-demo"
                 options={municipios}
+                value={selectedMunicipio}
+                isOptionEqualToValue={(option, value) => option === value}
                 sx={{ width: 300 }}
                 renderInput={(params) => (
                   <TextField {...params} label="Busque um município..." />
                 )}
+                onChange={handleMunicipio}
               />
             </div>
           </div>
+        </div>
+        <div className={styles.lowerDiv}>
+          <Chart
+            options={chartData.options}
+            series={chartData.series}
+            type="bar"
+            width={1000}
+            height={400}
+          />
         </div>
       </div>
     </div>
